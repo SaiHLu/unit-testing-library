7bdcd4fef058cf64ec48c4605ecd22759c3c8028
SaiHLu <saisailuhlaing@gmail.com>
2019-05-29 22:31:19 +0630

    Create BookReservationTest.php

--- tests/Unit/BookReservationTest.php
+++ tests/Unit/BookReservationTest.php
@@ -3,17 +3,70 @@
 namespace Tests\Unit;
 
 use Tests\TestCase;
+use App\Book;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
-class ExampleTest extends TestCase
+class BookReservationTest extends TestCase
 {
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function testBasicTest()
+    use RefreshDatabase;
+    /** @test */
+    public function a_book_can_be_added_to_the_library()
     {
-        $this->assertTrue(true);
+
+        $this->withoutExceptionHandling();
+
+        $response = $this->post('/books',
+            [
+                'title'=>'Cool Title',
+                'author'=>'shl',
+            ]);
+
+        $response->assertOk();
+        $this->assertCount(1, Book::all());
+
+    }
+
+    /** @test */
+    public function a_book_title_is_required()
+    {
+//        $this->withoutExceptionHandling();
+
+        $response = $this->post('/books',
+            [
+                'title'=> '',
+                'author'=>'shl'
+            ]);
+
+        $response->assertSessionHasErrors('title');
+    }
+
+    /** @test */
+    public function a_book_author_is_required()
+    {
+        $response = $this->post('/books', [
+            'title' => 'Cool Title',
+            'author' => ''
+        ]);
+
+        $response->assertSessionHasErrors('author');
+    }
+
+    /** @test */
+    public function a_book_can_be_updated()
+    {
+        $this->post('/books', [
+            'title' => 'Cool Title',
+            'author' => 'shl',
+        ]);
+
+        $book = Book::first();
+
+        $response = $this->patch('/books/'.$book->id, [
+           'title' => 'New Title',
+           'author' => 'New Author'
+        ]);
+
+        $this->assertEquals("New Title", Book::first()->title);
+        $this->assertEquals("New Author", Book::first()->author);
     }
 }
